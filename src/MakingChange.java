a0a6a8f2ecf4c4d48784d8190ac46e4b63820bc7
David Lutch <112986286+DL2026M@users.noreply.github.com>
2026-01-30 13:46:07 -0800

    Progress done in class

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -12,16 +12,27 @@ public class MakingChange {
      *  for any given total with any given set of coins.
      */
     public static long countWays(int target, int[] coins) {
+        long sum = 0;
+        long[][] coinsArray = new long[target + 1][coins.length];
+        return countRecursion(target, coins, coinsArray, coins.length);
 
-        // Case where there is only 1 type of coin
-        if (coins.length == 1) {
-            if (target % coins[0] == 0) {
-                return (target / coins[0]);
-            }
-            else {
-                return 0;
-            }
-        }
-        return 0;
     }
+    public static long countRecursion(int target, int[] coins, long[][] coinsArray, int sum) {
+        long sumTracker = 0;
+        if (sum == 0) {
+            return 1;
+        }
+        if (sum < 0) {
+            return 0;
+        }
+        // Out of bounds check
+        if (target < 0) {
+            return 0;
+        }
+        if (coinsArray[target][sum] == 0) {
+           // sumTracker = countRecursion((target - coins[sum]), coins, sum, sum) + countRecursion(target, coins, sum - 1, sum);
+            coinsArray[target][sum] = sumTracker;
+        }
+        return sumTracker;
+
 }
