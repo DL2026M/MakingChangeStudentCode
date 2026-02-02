acb394332bcad553294e350bc8c5fe8e07d78bbc
David Lutch <112986286+DL2026M@users.noreply.github.com>
2026-02-01 16:46:17 -0800

    Finished the whole problem set!

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -7,32 +7,57 @@
  */
 
 public class MakingChange {
-    /**
-     * TODO: Complete this function, countWays(), to return the number of ways to make change
-     *  for any given total with any given set of coins.
-     */
+    // Tabulation
     public static long countWays(int target, int[] coins) {
-        long sum = 0;
-        long[][] coinsArray = new long[target + 1][coins.length];
-        return countRecursion(target, coins, coinsArray, coins.length);
+        long[][] coinsArray = new long[coins.length + 1][target + 1];
+        for (int h = 0; h < coins.length; h++) {
+            // Base cases
+            coinsArray[h][0] = 1;
+        }
 
+        for (int i = 1; i <= coins.length; i++) {
+            for (int j = 1; j <= target; j++) {
+                long placeHolderValue;
+                // Getting the value above for the exclude case
+                placeHolderValue = coinsArray[i - 1][j];
+                // Only includes the coin if its value is less than or equal to the remaining sum
+                if (j >= coins[i - 1]) {
+                    placeHolderValue += coinsArray[i][j -  coins[i - 1]];
+                }
+                coinsArray[i][j] = placeHolderValue;
+            }
+        }
+        // The value in the bottom right of the table is the most amount of ways to make change given a target amount
+        // and a set of coins
+        return coinsArray[coins.length][target];
     }
-    public static long countRecursion(int target, int[] coins, long[][] coinsArray, int sum) {
+
+     // Memoization
+     /* public static long countWays(int target, int[] coins) {
+        long[][] coinsArray = new long[target + 1][coins.length];
+        return countRecursion(target, coins, coinsArray, coins.length-1);
+     }
+
+    public static long countRecursion(int leftOver, int[] coins, long[][] coinsArray, int index) {
         long sumTracker = 0;
-        if (sum == 0) {
+        if (leftOver == 0) {
             return 1;
         }
-        if (sum < 0) {
+        if (leftOver < 0) {
             return 0;
         }
         // Out of bounds check
-        if (target < 0) {
+        if (index < 0) {
             return 0;
         }
-        if (coinsArray[target][sum] == 0) {
-           // sumTracker = countRecursion((target - coins[sum]), coins, sum, sum) + countRecursion(target, coins, sum - 1, sum);
-            coinsArray[target][sum] = sumTracker;
+        if (coinsArray[leftOver][index] != 0) {
+            return coinsArray[leftOver][index];
+        }
+        // Only runs if this place in the 2d array hasn't been checked before
+        else {
+            sumTracker = countRecursion((leftOver - coins[index]), coins, coinsArray, index) + countRecursion(leftOver, coins, coinsArray, index - 1);
+            coinsArray[leftOver][index] = sumTracker;
         }
         return sumTracker;
-
+    }*/
 }
